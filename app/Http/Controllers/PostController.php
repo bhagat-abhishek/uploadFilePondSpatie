a5ebd7bbbb158dd087dd4508e93b6cd4fa1bf3d9
bhagat-abhishek <inst.abhishek@gmail.com>
2023-04-07 11:42:53 +0530

    Post Model, Migration and Controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+    public function index()
+    {
+        # code...
+    }
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function store(Request $request)
+    {
+        # code...
+    }
+
+    public function edit(Post $post)
+    {
+        # code...
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        # code...
+    }
+
+    public function destroy(Post $post)
+    {
+        # code...
+    }
+}
